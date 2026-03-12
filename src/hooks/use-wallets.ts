cb726d5418a1d57c3e1aa3592f388936ee6f54ed
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-03-12 15:27:08 +0600

    Enable external wallet mode

--- src/hooks/use-wallets.ts
+++ src/hooks/use-wallets.ts
@@ -3,7 +3,7 @@ import { useWalletStore } from '@/store/wallets-store'
 export const useAccounts = () => useWalletStore(state => state.accounts)
 export const useDisconnect = () => useWalletStore(state => state.disconnect)
 export const useConnectedWallets = () => useWalletStore(state => state.connectedWallets)
-export const useSelectedAccount = () => useWalletStore(state => state.selected)
+export const useSelectedAccount = () => useWalletStore(state => (state.externalWalletMode ? null : state.selected))
 export const useSelectAccount = () => useWalletStore(state => state.select)
 export const useExternalWalletMode = () => useWalletStore(state => state.externalWalletMode)
 export const useSetExternalWalletMode = () => useWalletStore(state => state.setExternalWalletMode)
@@ -11,7 +11,7 @@ export const useSetExternalWalletMode = () => useWalletStore(state => state.setE
 export const useWallets = () => {
   const accounts = useWalletStore(s => s.accounts)
   const isHydrated = useWalletStore(s => s.hasHydrated)
-  const selected = useWalletStore(s => s.selected)
+  const selected = useWalletStore(s => (s.externalWalletMode ? null : s.selected))
   const connectedWallets = useWalletStore(s => s.connectedWallets)
   const select = useWalletStore(s => s.select)
   const connect = useWalletStore(s => s.connect)
